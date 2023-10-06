48c3796e5d387c49d3c2297d5f1d06e9e3d30cc7
Sumat Mallick <115460605+SumatM@users.noreply.github.com>
2023-10-06 11:31:40 +0530

    new endpoint added

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const { apiRoute } = require("./route/api.route");
 
 const app = express();
 
+app.get('/',(req,res)=>{
+  try{
+    res.status(200).json({message:" User Sign-Up and Post Management API"})
+  }catch(err){
+    res.status(400).json({error:err})
+  }
+})
+
 app.use(express.json());
 
 app.use("/api", apiRoute);
